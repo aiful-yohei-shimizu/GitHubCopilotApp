09d7276d1316daefbebe1183e8070afd6d3efd17
aiful-yohei-shimizu <yohei.shimizu.aiful@gmail.com>
2024-08-28 09:07:35 +0900

    Storyboardを使用した画面遷移処理を実装

--- GitHubCopilotApp/ViewController.swift
+++ GitHubCopilotApp/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // buttonタップで、subViewControllerにpresentで遷移
+    // storyboardIDを利用する
+    @IBAction func buttonTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Sub", bundle: nil)
+        if let subVC = storyboard.instantiateViewController(withIdentifier: "SubViewController") as? SubViewController {
+            present(subVC, animated: true, completion: nil)
+        }
+    }
 
 }
 
