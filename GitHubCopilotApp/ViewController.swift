cfd2d5b26ddbf36a5a8d0e563d22d632b535ea64
aiful-yohei-shimizu <yohei.shimizu.aiful@gmail.com>
2024-08-28 10:50:34 +0900

    端末のモデル名、OSバージョン、名前、容量を取得してリスト化

--- GitHubCopilotApp/ViewController.swift
+++ GitHubCopilotApp/ViewController.swift
@@ -12,8 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        print(getDeviceInfo())
     }
 
-
+    // 端末のモデル名、OSバージョン、名前、容量を取得してリスト化する
+    func getDeviceInfo() -> [String] {
+        var deviceInfo: [String] = []
+        deviceInfo.append(UIDevice.current.model)
+        deviceInfo.append(UIDevice.current.systemVersion)
+        deviceInfo.append(UIDevice.current.name)
+        deviceInfo.append(UIDevice.current.systemName)
+        deviceInfo.append(UIDevice.current.identifierForVendor!.uuidString)
+        return deviceInfo
+    }
 }
 
