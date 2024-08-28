9d4536eb3f90e9da901528e30239053047784b4b
aiful-yohei-shimizu <yohei.shimizu.aiful@gmail.com>
2024-08-28 10:39:51 +0900

    ボタンタップでAPI通信を行い、画面遷移をする処理。API通信中はインジケーターを表示する

---
+++ GitHubCopilotApp/SubViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SubViewController.swift
+//  GitHubCopilotApp
+//
+//  Created by 清水陽平 on 2024/08/28.
+//
+
+import UIKit
+
+class SubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
